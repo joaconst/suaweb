4b55792b71e5fc51a6fa029a66fc0b5691b5a8e8
joacnt <constabeljoaquinn@gmail.com>
2025-03-05 16:30:23 -0300

    First Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#4a4a4a',
+      }
+    },
+  },
+  plugins: [],
+}
