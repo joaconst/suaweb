7862312999be2f704335b7fcf23a69f170dacd93
joacnt <constabeljoaquinn@gmail.com>
2025-03-12 20:44:21 -0300

    #

--- src/utils/button.ts
+++ src/utils/button.ts
@@ -19,17 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
             if (isExpanded) {
               initialContent.classList.add("opacity-0", "pointer-events-none");
               expandedContent.classList.remove("opacity-0", "pointer-events-none");
-              // Cambiar la altura de la card a 26rem en mobile
-              card.classList.remove("h-[20rem]");
-              card.classList.add("h-[26rem]");
               // Cambiar el título a "CARACTERÍSTICAS"
               cardTitle.innerText = "CARACTERÍSTICAS";
             } else {
               initialContent.classList.remove("opacity-0", "pointer-events-none");
               expandedContent.classList.add("opacity-0", "pointer-events-none");
-              // Volver la altura de la card a 20rem en mobile
-              card.classList.remove("h-[26rem]");
-              card.classList.add("h-[20rem]");
               // Cambiar el título de vuelta a "OBJETIVO"
               cardTitle.innerText = "OBJETIVO";
             }
